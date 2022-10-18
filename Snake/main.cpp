2dc969846a6def258411342cd39c5deca83dfcf6
Alexandru Chiriac <59575686+AlexandruChi@users.noreply.github.com>
2022-10-18 17:59:52 +0300

    Initial Commit

---
+++ Snake/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Snake
+//
+//  Created by Александру Кирияк on 18.10.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
